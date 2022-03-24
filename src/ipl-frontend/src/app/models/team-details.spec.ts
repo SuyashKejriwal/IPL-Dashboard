eb98f25ed1f0b5148f6f518ab2c1de78fb357821
SuyashKejriwal <suyashkejriwal1308@gmail.com>
2022-03-24 19:45:52 +0530

    deployed commit

--- src/ipl-frontend/src/app/models/team-details.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TeamDetails } from './team-details';
-
-describe('TeamDetails', () => {
-  it('should create an instance', () => {
-    expect(new TeamDetails()).toBeTruthy();
-  });
-});
--- src/main/java/com/example/ipldashboard/dto/MatchDto.java
+++ src/main/java/com/example/ipldashboard/dto/MatchDto.java
@@ -1,11 +1,9 @@
 package com.example.ipldashboard.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDate;
+
 
 public class MatchDto {
 
